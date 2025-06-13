fc56e417850cc79237c55aadf736c0631ee577c9
Mikhail Lebedev <softdrawart@gmail.com>
2025-06-13 12:39:53 +0300

    Update run.py
    player add

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
 import tkinter as tk
+from typing import List
 from tkinter import filedialog, colorchooser
 from PIL import Image, ImageTk #, ImageSequence
 from tkinterdnd2 import DND_FILES, TkinterDnD
@@ -14,23 +15,29 @@ class SpriteSheetToGif:
         self.root.title("Sprite Sheet to GIF")
 
         self.sprite_paths = []
+        self.current_path = ''
+        self.frames: List[Image.Image] = []
+        self.current_frame = 0
+
         self.slice_width = tk.IntVar(value=64)
         self.slice_height = tk.IntVar(value=64)
+        self.count_frames = tk.IntVar(value=1)
         self.fps = tk.IntVar(value=10)
         self.alpha_enabled = tk.BooleanVar(value=True)
         self.bg_color = "#ffffff"
+
 
         self.build_ui()
         self.run_player()
 
     def run_player(self):
-        selected_indicies = self.listbox.curselection()
-        if self.sprite_paths and len(selected_indicies)>0:
-            #if multiple selected only show first item
-            path = self.sprite_paths[selected_indicies]
-            if os.path.isfile(path) and path.lower().endswith((IMAGE_EXTENSIONS)):
-                image = Image.open(path)
-                self.image_tk = ImageTk.PhotoImage(image)
+        frames_count = len(self.frames)
+        if len(self.frames)!=0 and (frames_count > self.current_frame >= 0):
+            self.player.config(image=self.frames[self.current_frame])
+            self.player.image = self.frames[self.current_frame] #strong reference
+            self.current_frame = (self.current_frame + 1) % len(self.frames)
+        delay = int(1000/self.fps.get())
+        self.root.after(delay, self.run_player)
 
     def build_ui(self):
         #### LOAD IMAGES ####
@@ -42,19 +49,30 @@ class SpriteSheetToGif:
         self.listbox.drop_target_register(DND_FILES)
         self.listbox.dnd_bind('<<Drop>>', self.on_drop)
         self.listbox.bind('<Delete>', self.remove_selected_path)
+        self.listbox.bind('<<ListboxSelect>>', self.on_select)
         
 
         #### PLAYER ####
-        self.player = tk.Label(self.root, text="waiting for image...", bg='white', width=200, height=200)
+        blankImage = tk.PhotoImage(width=200, height=200)
+        self.player = tk.Label(self.root, image=blankImage, bg='white')
         self.player.pack()
         
 
         #### PARAMETERS ####
+        tk.Label(self.root, text="Frame Count").pack()
+        self.count = tk.Entry(self.root, textvariable=self.count_frames)
+        self.count.pack()
+        self.count.bind('<Return>', self.on_change_count)
+
         tk.Label(self.root, text="Frame Width").pack()
-        tk.Entry(self.root, textvariable=self.slice_width).pack()
+        self.width = tk.Entry(self.root, textvariable=self.slice_width)
+        self.width.pack()
+        self.width.bind('<Return>', self.on_resize)
 
         tk.Label(self.root, text="Frame Height").pack()
-        tk.Entry(self.root, textvariable=self.slice_height).pack()
+        self.height = tk.Entry(self.root, textvariable=self.slice_height)
+        self.height.pack()
+        self.height.bind('<Return>', self.on_resize)
 
         tk.Label(self.root, text="FPS (Frames per second)").pack()
         tk.Entry(self.root, textvariable=self.fps).pack()
@@ -65,6 +83,7 @@ class SpriteSheetToGif:
 
         tk.Button(self.root, text="Export as GIF", command=self.export_gif).pack()
         tk.Button(self.root, text="Export as PNG Frames", command=self.export_png_frames).pack()
+
 #####   EXTRA FUNCTIONS #####
     def slice_frames(self, path):
         image = Image.open(path)
@@ -86,7 +105,11 @@ class SpriteSheetToGif:
                 frames.append(frame)
 
         return frames
-        
+    def update_frames(self, path):
+        if os.path.isfile(path) and path.lower().endswith((IMAGE_EXTENSIONS)):
+            self.current_path = path
+            self.frames = [ImageTk.PhotoImage(img) for img in self.slice_frames(path)]
+            self.current_frame = 0 #restarts animation                
     def insert_files(self, paths: list[str]):
         for path in paths:
             if os.path.isfile(path) and path.lower().endswith((IMAGE_EXTENSIONS)):
@@ -111,11 +134,6 @@ class SpriteSheetToGif:
             self.bg_color = color
             print(f"Selected background color: {self.bg_color}")
 
-    def on_drop(self, event):
-        paths = self.root.tk.splitlist(event.data)
-        self.insert_files(paths)
-    def on_resize(self, event):
-        pass
     def export_gif(self):
         if not self.sprite_paths or len(self.sprite_paths) == 0:
             print("No sprite sheet loaded.")
@@ -142,6 +160,27 @@ class SpriteSheetToGif:
                 frame.save(filename)
             print(f"{len(frames)} PNG frames exported to: {output_dir}")
 
+#####   EVENT FUNCTIONS #####
+    def on_drop(self, event):
+        paths = self.root.tk.splitlist(event.data)
+        self.insert_files(paths)
+    def on_select(self, event):
+        if len(self.listbox.curselection())>0:
+            self.current_frame = 0 #restarts animation
+            path = self.sprite_paths[self.listbox.curselection()[0]]
+            if self.current_path == '' or self.current_path != path:
+                #if multiple selected only show first item
+                self.update_frames(path)
+    def on_resize(self, event):
+        self.update_frames(self.current_path)
+    def on_change_count(self, event):
+        path = self.current_path
+        if os.path.isfile(path) and path.lower().endswith((IMAGE_EXTENSIONS)):
+            image = Image.open(self.current_path)
+            img_w, img_h = image.size
+            self.slice_width.set(int(img_w/self.count_frames.get()))
+            self.slice_height.set(int(img_h))
+            self.update_frames(self.current_path)
 # Run GUI
 if __name__ == "__main__":
     root = TkinterDnD.Tk()
